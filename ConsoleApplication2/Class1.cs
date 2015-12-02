79efddb7b0349e6605ab52bd5c1bbac0c4521802
BryantVail <Bryanttv@outlook.com>
2015-12-01 21:22:05 -0500

    First upload from class
    assignment

---
+++ ConsoleApplication2/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Class1
+    {
+        string partDescription;
+        int quantity;
+        double price;
+        string partNumber;
+
+        //public Class1();
+
+        public Class1(string partDescriptionC, int quantityC,string partNumberC,double priceC)
+        {
+            partDescription = partDescriptionC;
+            Quantity = quantityC;
+            partNumber = partNumberC;
+            Price = priceC;
+
+        }
+        public string PartDescription
+        {
+            get
+            {
+                return partDescription;
+            }
+        }//end Property PartDescription
+
+        public string PartNumber
+        {
+            get
+            {
+                return partNumber;
+            }
+        }//end Property PartNumber
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if(value>0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    ArgumentOutOfRangeException priceException = new ArgumentOutOfRangeException("Price", "Price cannot be negative");
+                    throw priceException;
+                }//end if
+            }//end GetSet
+
+        }//end property Price
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                if (value > 0)
+                    quantity = value;
+                else
+                {
+                    ArgumentOutOfRangeException quantityException = new ArgumentOutOfRangeException();
+                    throw quantityException;
+                }
+            }//end if
+        }//end PRoperty Quantity
+
+
+    }//end class
+}//end namespace
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1[] PartList = new Class1[8];
+           PartList[0] = new Class1("electric Sander", 7,"83", 57.95 );
+           PartList[1] = new Class1("Power Saw", 18,"24", 99.99 );
+           PartList[2] = new Class1("Sledge Hammer", 11,"7", 21.50 );
+           PartList[3] = new Class1("Hammer", 76, "77", 11.99 );
+           PartList[4] = new Class1("Lawn Mower", 3, "39",79.50 );
+           PartList[5] = new Class1("Jug Saw", 21, "56", 11.00 );
+           PartList[6] = new Class1("Screwdriver", 106, "68", 6.99 );
+                   
+           PartList[7] = new Class1( "Wrench", 34,"3",7.50);
+
+            List<Class1> PowerTools = new List<Class1>();
+           
+            PowerTools.Add(PartList[0]);
+            PowerTools.Add(PartList[1]);
+            PowerTools.Add(PartList[2]);
+            PowerTools.Add(PartList[3]);
+            PowerTools.Add(PartList[4]);
+            PowerTools.Add(PartList[5]);
+            PowerTools.Add(PartList[6]);
+            PowerTools.Add(PartList[7]);
+
+            var PriceList=
+                from tool in PowerTools
+                orderby tool.Price
+                select PowerTools;
+
+            var quantityCheck =
+                from tool in PowerTools
+                orderby tool.PartDescription
+                select new {quantityCheck = QuantityResult };
+            int quantitycheck = quantityCheck;
+                Console.WriteLine(PriceList +":"+ quantityCheck);
+            bool loop = true;
+            //while(loop == true)
+            Console.Read();
+
+
+
+        }//end main
+
+    }//end class
+}//end namespace
